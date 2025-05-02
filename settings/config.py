f9d386d40a1697f37989874db5252530f671d3d9
Chelsyshankiri <cs688@njit.edu>
2025-05-02 02:56:24 -0400

    Added SMTP details for sending emails

--- settings/config.py
+++ settings/config.py
@@ -38,8 +38,8 @@ class Settings(BaseSettings):
     # Email settings for Mailtrap
     smtp_server: str = Field(default='smtp.mailtrap.io', description="SMTP server for sending emails")
     smtp_port: int = Field(default=2525, description="SMTP port for sending emails")
-    smtp_username: str = Field(default='your-mailtrap-username', description="Username for SMTP server")
-    smtp_password: str = Field(default='your-mailtrap-password', description="Password for SMTP server")
+    smtp_username: str = Field(default='9a60a803bba2cd', description="Username for SMTP server")
+    smtp_password: str = Field(default='34f46ee1833d82', description="Password for SMTP server")
 
 
     class Config:
